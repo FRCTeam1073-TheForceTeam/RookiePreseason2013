8c11f0d985529f0f38ca6cf51973d3d969eaca51
TomVlatas <tommyvlatas@gmail.com>
2013-10-23 20:25:46 -0400

    updated LED code

--- 20131023/Tommy and Matt/ex1.ino
+++ 20131023/Tommy and Matt/ex1.ino
@@ -4,24 +4,30 @@
 int delayTime = 1000; // ms
 int LED = 3;
 int chooseLED = 2;
+int isError = 0;
 
 void setup() {
   Serial.begin(9600);
-    setupLed(LED);
+    setupLed(GREEN_LED);
+    setupLed(ONBOARD_LED);
 }
 
 void loop() {
   if(chooseLED == 1){
-      LED = 13;
+    LED = 13;
     }
-    if(chooseLED == 2){
-      LED = 3;
+  if(chooseLED == 2){
+    LED = 3;
     }
-    else {
-      Serial.println("invalid key stroke.....stupid");
-    }
+    
   while(Serial.available() > 0) {
-    chooseLED = Serial.parseInt();
+    int input = Serial.parseInt();
+    if(input == 1 || input == 2){
+      chooseLED = input;
+    }
+    else{
+      delayTime = input;
+    }
     if (Serial.read() == '\n') {
       // do nothing, just munch the carriage return
     }
