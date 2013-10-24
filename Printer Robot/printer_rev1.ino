4b9d7f489079784db485aa2d3f9838459cce000e
Patrick Clark <pac3013564@gmail.com>
2013-10-23 20:09:44 -0400

    Added "L" Command
    Draws the letter "L" and then stops

--- Printer Robot/printer_rev1.ino
+++ Printer Robot/printer_rev1.ino
@@ -4,15 +4,22 @@ Servo clawServo, forearmServo, armServo, turretServo, leftDrive, rightDrive;
 int clawLed;
 int statusLed = 11;
 char serialVal = 0;
+void write_l();
+
 void setup() {
-  //clawServo.attach();
-  //forearmServo.attach();
-  /*armServ.attach();
-  turretServo.attach();*/
+  clawServo.attach(0);
+  forearmServo.attach(0);
+  armServo.attach(0);
+  turretServo.attach(0);
   leftDrive.attach(6);
   rightDrive.attach(4);
   pinMode(statusLed, OUTPUT);
   digitalWrite(statusLed, HIGH);
+  clawServo.write(0);
+  forearmServo.write(0);
+  armServo.write(0);
+  turretServo.write(0);
+  write_l();
 }
 
 void loop() {
@@ -21,10 +28,7 @@ void loop() {
   }
   serialVal = Serial.read();
  // driveRobot(Serial.read());
-
-
-//void driveRobot(char serialVal) {
-Serial.println(serialVal);
+ Serial.println(serialVal);
   if (serialVal == 'w') {
     leftDrive.writeMicroseconds(1706);
     rightDrive.writeMicroseconds(1300);
@@ -43,3 +47,30 @@ Serial.println(serialVal);
     rightDrive.writeMicroseconds(1500);
   }
 }
+ 
+
+void write_l() { 
+ // Letter L
+ forearmServo.write(20);
+ leftDrive.writeMicroseconds(1706);
+ rightDrive.writeMicroseconds(1300);
+ delay(500);
+ rightDrive.writeMicroseconds(1700);
+ forearmServo.write(0);
+ delay(1500); // How fast is the robot?
+ leftDrive.writeMicroseconds(1306);
+ delay(500);
+ leftDrive.writeMicroseconds(1706);
+ delay(1000);
+ leftDrive.writeMicroseconds(1506);
+ rightDrive.writeMicroseconds(1500);
+ // Robot should now be stopped
+} 
+ 
+ 
+ 
+
+
+//void driveRobot(char serialVal) {
+
+
