b12ac4c3be66990f8d7fd27f675b15d4bf7206fc
TheCoalBurner <I.Colburn123@gmail.com>
2013-10-23 20:31:07 -0400

    Added a for loop

--- 20131023/Ian/Ian.ino
+++ 20131023/Ian/Ian.ino
@@ -1,6 +1,7 @@
 #include "led.h"
 
 int delayTime = 200; // ms
+int flashAmount = 5; //default
 
 void setup() {
   Serial.begin(9600);
@@ -8,23 +9,35 @@ void setup() {
 }
 
 void loop() {
+  int i;
+  int tempDelayTime = 0; 
   while(Serial.available() > 0) {
-    int tempDelayTime = Serial.parseInt();
-    
+     tempDelayTime = Serial.parseInt();
+  }  
     if (tempDelayTime == 0) {
       while(Serial.available() > 0) {
-        int theLed = Serial.parseInt();
+        theLed = Serial.parseInt();
+      }
+      while(Serial.available() > 0) {
+        int flashAmount = Serial.parseInt();
+      }
         if (theLed == 1)
           setupLed(ONBOARD_LED);
         else if (theLed == 2)
           setupLed(GREEN_LED);
         else
           Serial.write("Please enter 1 for onboard, 2 for green\n");
-      }
-    }
+          
+         }
     if (Serial.read() == '\n') {
       // do nothing, just much the carriage return
     }
+  for(i = 0; i < flashAmount; i++){
+    flashLed(delayTime);
+    
   }
-  flashLed(delayTime);
+  
+  delay(1000); 
+    
+
 }
--- 20131023/Ian/led.h
+++ 20131023/Ian/led.h
@@ -1,5 +1,7 @@
 #define GREEN_LED 4
 #define ONBOARD_LED 13
 
+extern int theLed;
+
 void setupLed(int whichLed);
 void flashLed(int dtime);
