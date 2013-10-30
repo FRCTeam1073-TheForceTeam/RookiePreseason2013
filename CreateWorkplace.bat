9d9b947acbb65350a523204d7edba5ab90d4f24c
Evin Ugur <evinugur96@gmail.com>
2013-10-30 14:07:57 -0400

    Added in explanation text

--- CreateWorkplace.bat
+++ CreateWorkplace.bat
@@ -5,4 +5,12 @@ mkdir %curr_date:-=%
 cd %curr_date:-=%
 mkdir %username%
 cd %user_name%
+
+cls
+title Creating workspace for %username%... 
+echo.
+echo.
+echo Your environment has been created, for your convenience please place your code for tonight's project in this directory. This makes it easy to get to your work even if you switch computers, and to check in on the cool stuff your friends are working on!
+pause
+
 explorer .
