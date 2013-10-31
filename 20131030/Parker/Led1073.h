458cbf37733f3cd0230ce580bdb577dbf058234d
coadyp <parker@thecoadys.com>
2013-10-30 20:10:51 -0400

    Did excercise
    Compiled everything into one folder, changed the onboard LED from pin 3
    into pin 4

---
+++ 20131030/Parker/Led1073.h
@@ -0,0 +1,18 @@
+#define GREEN_LED 4
+#define ONBOARD_LED 13
+
+class Led1073 {
+private:
+  static const int fastDelayDflt = 500;
+  static const int slowDelayDflt = 1000;
+  int whichLed;
+  int fastDelayTime;
+  int slowDelayTime;
+  
+public:
+  Led1073(int pin, int fastDelay = fastDelayDflt, int slowDelay = slowDelayDflt);
+  void enable(void);
+  void setFastDelay(int fastDelay);
+  void setSlowDelay(int slowDelay);
+  void flashLed(int flashCount = 1);
+};
